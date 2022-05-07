b793a84cd5481aef5b346ab9b42dbecbbf28c8e3
Tarq87 <99237045+Tarq87@users.noreply.github.com>
2022-05-07 13:39:54 +0300

    Delete Back-end directory

--- Back-end/config/dbConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    secret: 'yoursecret',
-    //Link to the database, change when going to the cloud(atlas)
-    database: 'mongodb+srv://Ammar:QweAsdZxc@testing.xymiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
