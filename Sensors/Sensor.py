169d1e85924ef3ba103661c2915a613c51492703
Tarq87 <bnnna3@gmail.com>
2022-05-11 23:34:54 +0300

    1st

--- Sensors/Sensor.py
+++ Sensors/Sensor.py
@@ -7,13 +7,12 @@ sensor = DistanceSensor(echo=18, trigger=17)
 while True:
     Dis=sensor.distance
     t=time()
-    print('Distance: ',Dis, '                  ',ctime(t) )
+    print('Distance: ',Dis, '                   time',ctime(t) )
     if (Dis<0.2):
-        counter=counter+1
+        
         td=ctime(t)
-        payload={"td": td, "loc": counter}
-        print('Distance: ',Dis, '  time  :',ctime(t),"count ",t )
+        payload={"td": td}
+        print('Passenger Exit the bus: ',Dis, '   time  :',ctime(t))
         r= requests.post('https://db730626-6988-4ac5-afec-67b96f419fab.mock.pstmn.io/cv/qw',json=payload)
         print (r.text)
     sleep(0.4)
-    
